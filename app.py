1512865077ee29505c223c8ac597ce27193a18fc
john-blaine <john.alan.blaine@gmail.com>
2020-06-22 22:08:42 -0600

    flesh out implementation of routes in flask app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -14,13 +14,24 @@ stores = [
   }
 ]
 
+def find_store(name):
+  for store in stores:
+    if store['name'] == name:
+      return store
+
 @app.route('/store', methods=['POST'])
 def create_store():
-  pass
+  request_data = request.get_json()
+  new_store = {
+    'name': request_data['name'],
+    'items': []
+  }
+  stores.append(new_store)
+  return jsonify(new_store)
 
 @app.route('/store/<string:name>')
 def get_store(name):
-  pass
+  return jsonify(find_store(name) or { 'message': 'store not found' })
 
 @app.route('/store')
 def get_stores():
@@ -28,10 +39,19 @@ def get_stores():
 
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
-  pass
+  found_store = find_store(name)
+  if (found_store == None):
+    return jsonify({ 'message': 'store not found' })
+  request_data = request.get_json()
+  new_item = {
+    'name': request_data['name'],
+    'price': request_data['price']
+  }
+  found_store['items'].append(new_item)
+  return jsonify(new_item)
 
 @app.route('/store/<string:name>/item')
 def get_items_in_store(name):
-  pass
+  return jsonify( find_store(name)['items'] or { 'message': 'store not found' })
 
 app.run(port=5000)
